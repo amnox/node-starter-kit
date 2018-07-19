70ad1ab9b2ee233fd276c131b5f5f108f37a4c21
amnox <khalid_aman@ymail.com>
2018-07-19 21:17:48 +0530

    Youtube Tutorial notes

--- 1hr Video/first/app.js
+++ 1hr Video/first/app.js
@@ -1,5 +1,37 @@
+/* 
+    1. Hello World 
+*/
+
 function hello(name) {
     console.log('hi '+name)
 }
 
-hello('amnox')
+//hello('amnox')
+
+
+/* 
+    2. Scopes in node 
+*/
+
+// This would not work because global variables in Node are different from browser environment
+//console.log(window)
+// Instead
+//console.log(module)
+
+
+/* 
+    3. Running an exported module 
+*/ 
+
+const logger = require('./logger')
+//logger.log('message')
+
+
+
+/*
+    4. Path module
+*/
+
+const path = require('path')
+
+console.log(path.parse(__filename))
---
+++ 1hr Video/first/logger.js
@@ -0,0 +1,13 @@
+var url = 'http://google.com'
+
+function log(message) {
+    console.log(message)
+}
+
+module.exports.log = log;
+module.exports.endPoint = url;
+
+//console.log(module)
+
+
+
