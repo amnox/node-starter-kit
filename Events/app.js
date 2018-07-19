0927d5e46b5760046aad231dd4cbbd5fc9f59f9d
amnox <khalid_aman@ymail.com>
2018-07-19 22:35:21 +0530

    Basic Event Emitter

---
+++ Events/app.js
@@ -0,0 +1,16 @@
+
+const EventEmitter = require('events')
+const emitter = new EventEmitter();
+
+/**
+ * Register a listener
+ */
+emitter.on('messageLogged', (...args)=>{
+    console.log(args);
+})
+
+/**
+ * Raise an event
+ */
+emitter.emit('messageLogged', 1, 'url')
+
